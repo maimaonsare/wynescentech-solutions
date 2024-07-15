3fc5548c4a492379c7f3813506a12c9a69a1da8d
Vincent <maimahvincent@gmail.com>
2024-07-15 10:45:37 +0300

    Updates

---
+++ precache-manifest.cc8d54121237b12198f4436614eed98d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d1becbe29f4fdf65dc5bcb37d6ef0c",
+    "url": "/wynescentech-solutions/index.html"
+  },
+  {
+    "revision": "7b906287988a502e2bd0",
+    "url": "/wynescentech-solutions/static/css/main.80d96a84.chunk.css"
+  },
+  {
+    "revision": "53681b75d78a176e5631",
+    "url": "/wynescentech-solutions/static/js/2.df8c090d.chunk.js"
+  },
+  {
+    "revision": "7b906287988a502e2bd0",
+    "url": "/wynescentech-solutions/static/js/main.85009de3.chunk.js"
+  },
+  {
+    "revision": "f1e04aa374a9bf3d376d",
+    "url": "/wynescentech-solutions/static/js/runtime~main.0a86a57d.js"
+  },
+  {
+    "revision": "ce404b15cf1fe0c90b80ae9fb54057af",
+    "url": "/wynescentech-solutions/static/media/bg.ce404b15.PNG"
+  },
+  {
+    "revision": "8c9e76ed71d211d0da3c9265de18de72",
+    "url": "/wynescentech-solutions/static/media/bg_sm.8c9e76ed.PNG"
+  },
+  {
+    "revision": "09e7c9fd2e17791379d3583bbaebca91",
+    "url": "/wynescentech-solutions/static/media/design.09e7c9fd.jpg"
+  },
+  {
+    "revision": "b800dff5aff91ab703d90a872de443ea",
+    "url": "/wynescentech-solutions/static/media/engine.b800dff5.jpg"
+  }
+]);
